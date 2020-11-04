1c3846a3c86a987fa28173b2786fd23feee72fdf
Amit Joshi <amit8439941788@gmail.com>
2020-11-04 11:56:06 +0530

    latest commit

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,2 +1,19 @@
-print("amit joshi")
+list=[]
+num=None
+while(True):
+    num=input("Enter your input")
+    if num=='done':
+       break
+
+    try:
+        num==int(num)
+        list.append(num)
+
+    except:
+        print("INVALID INPUT")
+
+print("Maximum number is",max(list))
+print("Minimum number is",min(list))
+
+
 
--- helli.py
+++ helli.py
@@ -1,4 +1,2 @@
-import os
-list1 =[1, 2, 8, 9, 4, 5]
-list1.sort()
-print("sorted lsit",list1)
+print("Amitjoshi")
+print("python file")
